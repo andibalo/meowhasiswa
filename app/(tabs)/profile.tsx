e4dd081d815542ae0dd9a4b35c597e5458264f01
MG42freak <mg42freak@gmail.com>
2024-11-11 23:51:20 +0700

    First commit
    Random Bullshit Go

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,51 @@
-import { Text, View } from 'tamagui'
+import { Text, View, Image } from 'tamagui'
 
 export default function ProfileScreen() {
   return (
-    <View flex={1} alignItems="center" justifyContent="center">
-      <Text fontSize={20} color="$blue10">
-        Profile
-      </Text>
+    <View flex={1} backgroundColor="$white" padding={20}>
+      <View alignItems="center" marginBottom={10}>
+        <Text fontSize={35} color="$black" paddingLeft={10}>
+          Profile
+        </Text>
+      </View>
+
+      <View flexDirection="row" justifyContent="space-between" alignItems="center" paddingVertical={10} borderBottomWidth={1} borderBottomColor="$gray">
+        <View flexDirection="row" alignItems="center">
+          <Image
+            source={require('./assets/placeholder.png')}
+            width={50}
+            height={50}
+            borderRadius={25}
+          />
+          <View marginLeft={10}>
+            <Text fontSize={16} color="$black" fontWeight="bold">
+              Kampus
+            </Text>
+            <Text fontSize={14} color="$gray">
+              Username
+            </Text>
+            <Text fontSize={14} color="$gray">
+              Prodi
+            </Text>
+          </View>
+        </View>
+
+        <Text fontSize={14} color="$gray">
+          Joined Date
+        </Text>
+      </View>
+
+      <View flexDirection="row" justifyContent="center" marginVertical={10}>
+        <Text fontSize={16} fontWeight="bold" marginHorizontal={20} textDecorationLine="underline">
+          Profile
+        </Text>
+        <Text fontSize={16} marginHorizontal={20} color="$gray">
+          Posts
+        </Text>
+        <Text fontSize={16} marginHorizontal={20} color="$gray">
+          Comments
+        </Text>
+      </View>
     </View>
   )
 }
--- app/(tabs)/university.tsx
+++ app/(tabs)/university.tsx
@@ -1,9 +1,37 @@
+import { View, Text, Image } from 'tamagui'
 import { ReviewList } from 'components/university'
-import { View } from 'tamagui'
+import { FontAwesome } from '@expo/vector-icons'
 
 export default function UniversityScreen() {
   return (
-    <View p={'$3'} pb={'$0'} >
+    <View flex={1} padding={'$3'} backgroundColor="$white">
+      <View alignItems="center" marginBottom={'$3'}>
+        <Text fontSize={35} color="$black">
+          University
+        </Text>
+      </View>
+
+      <View flexDirection="row" alignItems="center" marginBottom={'$3'}>
+        <Image
+          source={require('./assets/placeholder.png')}
+          width={50}
+          height={50}
+          borderRadius={25}
+        />
+        <View marginLeft={'$3'}>
+          <Text fontSize={16} fontWeight="bold" color="$black">
+            UMN
+          </Text>
+          <Text fontSize={14} color="$gray">
+            6 hours ago
+          </Text>
+          <Text fontSize={14} color="$gray">
+            Informatika
+          </Text>
+        </View>
+      </View>
+
+      {/* Review List */}
       <ReviewList />
     </View>
   )
