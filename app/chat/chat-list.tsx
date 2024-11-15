31fa1833539c5b16c3e59ab184856507c722e60b
Rassel <rassel.billiono@student.umn.ac.id>
2024-11-15 19:46:47 +0700

    chat-list

--- app/chat/chat-list.tsx
+++ app/chat/chat-list.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { Text, View } from "tamagui";
-import { FlatList, Image } from "react-native";
+import { FlatList } from "react-native";
+import { SizableText, XStack, YStack, Image, Avatar } from "tamagui";
 
 const messages = [
   {
     id: "1",
     university: "UBM",
     username: "tolecat",
-    text: "Hey, how are you?",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel augue nisl. Vestibulum pellentesque ante ex, eu molestie nulla malesuada eget. Nullam dapibus sit amet erat ac ornare.",
     timestamp: "Kemarin",
     profilePic:
       "https://pacificgarden.co.id/wp-content/uploads/2021/10/Logo-UBM-Universitas-Bunda-Mulia-Original-1024x744.png",
@@ -16,7 +15,7 @@ const messages = [
     id: "2",
     university: "Untar",
     username: "catlover",
-    text: "I am good, thanks! How about you?",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel augue nisl. Vestibulum pellentesque ante ex, eu molestie nulla malesuada eget. Nullam dapibus sit amet erat ac ornare.",
     timestamp: "12/03/2024",
     profilePic: "https://registrasi.untar.ac.id/assets/images/logo_untar.png",
   },
@@ -24,54 +23,49 @@ const messages = [
 
 export default function ChatListScreen() {
   const renderItem = ({ item }) => (
-    <View
-      flexDirection="row"
-      padding={10}
-      marginBottom={10}
-      backgroundColor="$background"
-      borderRadius={10}
-      shadowColor="$shadow"
-      shadowOffset={{ width: 0, height: 2 }}
-      shadowOpacity={0.1}
-      shadowRadius={4}
-    >
-      <View flexDirection="row" flex={1} alignItems="center">
-        <Image
-          source={{ uri: item.profilePic }}
-          style={{
-            width: 40,
-            height: 40,
-            borderRadius: 0,
-            marginRight: 10,
-            borderWidth: 2,
-            borderColor: "black",
-          }}
-        />
-        <View>
-          <View flexDirection="row" alignItems="center">
-            <Text fontSize={15} fontWeight="bold" color="$primary">
+    <XStack padding="$1" marginBottom="$4" alignItems="center">
+      <XStack flex={1} alignItems="center">
+        <Avatar
+          borderRadius={"$2"}
+          borderWidth="$1"
+          borderColor="$primary"
+          marginRight="$2"
+          size="$5"
+        >
+          <Avatar.Image
+            accessibilityLabel="Cam"
+            src={item.profilePic}
+            objectFit="contain"
+          />
+          <Avatar.Fallback backgroundColor="$secondary" />
+        </Avatar>
+        <YStack flex={1}>
+          <XStack alignItems="center">
+            <SizableText size="$5" fontWeight="bold" color="$primary">
               {item.university}
-            </Text>
-            <Text fontSize={12} marginLeft={5} color="$primary">
+            </SizableText>
+            <SizableText size="$3" marginLeft="$2" color="$secondary">
               {item.username}
-            </Text>
-          </View>
-          <Text color="$secondary">{item.text}</Text>
-        </View>
-      </View>
-      <Text fontSize={12} color="$tertiary">
+            </SizableText>
+          </XStack>
+          <SizableText size="$4" color="$primary" numberOfLines={1}>
+            {item.text}
+          </SizableText>
+        </YStack>
+      </XStack>
+      <SizableText size="$1" color="$secondary">
         {item.timestamp}
-      </Text>
-    </View>
+      </SizableText>
+    </XStack>
   );
-  
+
   return (
-    <View flex={1} padding={20} backgroundColor="$background">
+    <YStack flex={1} padding="$4" backgroundColor="$background">
       <FlatList
         data={messages}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
       />
-    </View>
+    </YStack>
   );
 }
