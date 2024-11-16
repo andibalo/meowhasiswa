39cb1195836893bdc9b1e477fcda4b74c9c7564a
MG42freak <mg42freak@gmail.com>
2024-11-16 12:09:50 +0700

    Update the coloration on create thread button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -179,7 +179,7 @@ export default function HomeScreen() {
           position: 'absolute',
           bottom: 20,
           right: 20,
-          backgroundColor: '$primary',
+          backgroundColor: '$secondary',
           width: 60,
           height: 60,
           borderRadius: 30,
