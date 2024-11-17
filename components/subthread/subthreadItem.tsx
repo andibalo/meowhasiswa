559116a3be1d07753e451769bd9feffd95a38dc0
sylphhhhhh <meshach.christ@gmail.com>
2024-11-17 21:48:12 +0700

    final subthreadlist

--- components/subthread/subthreadItem.tsx
+++ components/subthread/subthreadItem.tsx
@@ -15,7 +15,7 @@ export const SubthreadItem = (props: SubThreadItemProps) => {
                         <Avatar borderRadius={'$2'} borderWidth="$1" borderColor="$primary" size="$4">
                             <Avatar.Image
                                 accessibilityLabel="Cam"
-                                src={Subthread.university_image_url}
+                                src={Subthread.image_url}
                                 objectFit="contain"
                             />
                             <Avatar.Fallback backgroundColor="$secondary" />
@@ -23,20 +23,20 @@ export const SubthreadItem = (props: SubThreadItemProps) => {
                     </View>
                     <View flex={1}>
                         <XStack flex={1} justifyContent='space-between'>
-                            <Text color="$primary" fontWeight="bold">m/{Subthread.subthread_name}</Text>
+                            <Text color="$primary" fontWeight="bold">m/{Subthread.name}</Text>
                             <Button bg="$backgroundSoft" padding="$1" borderRadius="$3" width={100} height={24} >
                                 <Text fontSize="$2"> Follow </Text>
                             </Button>
                         </XStack>
                         <XStack gap="$2" alignItems="center">
-                            <Text color="$primary" fontSize="$3">{Subthread.subthread_follower}</Text>
+                            <Text color="$primary" fontSize="$3">{Subthread.followers_count}</Text>
                         </XStack>
                     </View>
                 </XStack>
             </XStack>
             <YStack pr={'$3'} pl={'$3'} pb={'$1.5'} gap="$1">
                 <Text color="$primary">
-                    {Subthread.subthread_description}
+                    {Subthread.description}
                 </Text>
             </YStack>
         </View>
--- types/model/subthread.ts
+++ types/model/subthread.ts
@@ -1,21 +1,12 @@
 export interface ISubThread {
     id: string;
-    user_id: string;
-    username: string;
-    university_abbreviated_name: string;
-    university_image_url: string;
-    subthread_id: string;
-    subthread_name: string;
-    subthread_color: string;
-    subthread_follower: string;
-    subthread_description: string;
-    title: string;
-    content: string;
-    content_summary: string;
-    is_active: boolean;
-    like_count: number;
-    dislike_count: number;
-    comment_count: number;
+    name: string;
+    followers_count: string;
+    description: string;
+    image_url: string;
+    label_color: string;
+    university_id: string | null;
+    is_university_subthread: string,
     created_by: string;
     created_at: string;
     updated_by: string | null;
