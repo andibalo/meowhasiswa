6d7e8dc7955f1d4357db0bf954d021a7c843d8ab
Rassel <rassel.billiono@student.umn.ac.id>
2024-12-04 10:19:04 +0700

    add edit subthread screen placeholder

---
+++ app/(protected)/subthread/edit-subthread.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function EditSubthreadScreen() {
+  return (
+    <View>
+      <Text>edit-subthread</Text>
+    </View>
+  )
+}
